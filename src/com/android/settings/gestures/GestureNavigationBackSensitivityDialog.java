RevengeOS/android_packages_apps_Settings

Allow navigation gesture bar to be hidden [2/2]
Navigation gesture bar takes 16dp space on the screen, allow the user
to hide it for extra screen real estate

Change-Id: I9dd687c7461c2726632b4560a7a2c1e636c13fc2
Signed-off-by: Lucchetto <lucchetto.tie@live.com>

src/com/android/settings/gestures/GestureNavigationBackSensitivityDialog.java
@@ -16,18 +16,25 @@
 
 package com.android.settings.gestures;
 
+import static android.os.UserHandle.USER_CURRENT;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.settings.SettingsEnums;
 import android.content.Context;
 import android.content.om.IOverlayManager;
+import android.content.om.OverlayInfo;
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.SeekBar;
+import android.widget.Switch;
 
 import com.android.settings.R;
 import com.android.settings.core.instrumentation.InstrumentedDialogFragment;
+
+import com.revengeos.internal.util.NavBarUtils;
 
 /**
  * Dialog to set the back gesture's sensitivity in Gesture navigation mode.
@@ -57,10 +64,21 @@ public class GestureNavigationBackSensitivityDialog extends InstrumentedDialogFr
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final IOverlayManager overlayManager = IOverlayManager.Stub.asInterface(ServiceManager.getService(Context.OVERLAY_SERVICE));
         final View view = getActivity().getLayoutInflater().inflate(
                 R.layout.dialog_back_gesture_sensitivity, null);
         final SeekBar seekBar = view.findViewById(R.id.back_sensitivity_seekbar);
         seekBar.setProgress(getArguments().getInt(KEY_BACK_SENSITIVITY));
+        Switch mNavBarGesturalHideNav = view.findViewById(R.id.nav_bar_gestural_hide_nav_switch);
+        boolean mIsGesturalNavBarHidden = NavBarUtils.isGesturalNavBarHidden(getContext(), USER_CURRENT);
+        if (SystemNavigationPreferenceController.isEdgeToEdgeEnabled(getContext())) {
+            OverlayInfo ovInfo = null;
+            try {
+                ovInfo = overlayManager.getOverlayInfo(NavBarUtils.NAV_BAR_GESTURAL_HIDE_NAV_OVERLAY, USER_CURRENT);
+            } catch (RemoteException e) { }
+            mIsGesturalNavBarHidden = mIsGesturalNavBarHidden && ovInfo != null && (ovInfo.state == OverlayInfo.STATE_ENABLED);
+        }
+        mNavBarGesturalHideNav.setChecked(NavBarUtils.isGesturalNavBarHidden(getContext(), USER_CURRENT));
         return new AlertDialog.Builder(getContext())
                 .setTitle(R.string.back_sensitivity_dialog_title)
                 .setMessage(R.string.back_sensitivity_dialog_message)
@@ -69,12 +87,15 @@ public class GestureNavigationBackSensitivityDialog extends InstrumentedDialogFr
                     int sensitivity = seekBar.getProgress();
                     getArguments().putInt(KEY_BACK_SENSITIVITY, sensitivity);
                     SystemNavigationGestureSettings.setBackSensitivity(getActivity(),
-                            getOverlayManager(), sensitivity);
+                            overlayManager, sensitivity);
+                    final boolean mNavBarGesturalHideNavEnabled = mNavBarGesturalHideNav.isChecked();
+                    if (SystemNavigationPreferenceController.isEdgeToEdgeEnabled(getContext())) {
+                        if (NavBarUtils.setGesturalNavBarHiddenOverlay(overlayManager, USER_CURRENT, mNavBarGesturalHideNavEnabled)) {
+                            Settings.System.putIntForUser(getContext().getContentResolver(),
+                                    Settings.System.NAV_BAR_GESTURAL_HIDE_NAV, mNavBarGesturalHideNavEnabled ? 1 : 0, USER_CURRENT);
+                        }
+                    }
                 })
                 .create();
-    }
-
-    private IOverlayManager getOverlayManager() {
-        return IOverlayManager.Stub.asInterface(ServiceManager.getService(Context.OVERLAY_SERVICE));
     }
 }

src/com/android/settings/gestures/SystemNavigationGestureSettings.java
@@ -56,6 +56,8 @@ import com.android.settings.widget.VideoPreference;
 import com.android.settingslib.search.SearchIndexable;
 import com.android.settingslib.widget.CandidateInfo;
 
+import com.revengeos.internal.util.NavBarUtils;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -285,12 +287,15 @@ public class SystemNavigationGestureSettings extends RadioButtonPickerFragment {
             case KEY_SYSTEM_NAV_GESTURAL:
                 int sensitivity = getBackSensitivity(context, overlayManager);
                 setNavBarInteractionMode(overlayManager, BACK_GESTURE_INSET_OVERLAYS[sensitivity]);
+                NavBarUtils.setGesturalNavBarHiddenOverlay(overlayManager, USER_CURRENT, NavBarUtils.isGesturalNavBarHidden(context, USER_CURRENT));
                 break;
             case KEY_SYSTEM_NAV_2BUTTONS:
                 setNavBarInteractionMode(overlayManager, NAV_BAR_MODE_2BUTTON_OVERLAY);
+                NavBarUtils.setGesturalNavBarHiddenOverlay(overlayManager, USER_CURRENT, false);
                 break;
             case KEY_SYSTEM_NAV_3BUTTONS:
                 setNavBarInteractionMode(overlayManager, NAV_BAR_MODE_3BUTTON_OVERLAY);
+                NavBarUtils.setGesturalNavBarHiddenOverlay(overlayManager, USER_CURRENT, false);
                 break;
         }
     }

pxnoswip/packages_apps_Settings

Signed-off-by: blinoff82 <blinov.in@gmail.com>
Signed-off-by: drpradsms <dr.pradsms@gmail.com>

src/com/android/settings/gestures/GestureNavigationBackSensitivityDialog.java
@@ -16,12 +16,15 @@
 
 package com.android.settings.gestures;
 
+import static android.os.UserHandle.USER_CURRENT;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.settings.SettingsEnums;
 import android.content.Context;
 import android.content.om.IOverlayManager;
+import android.content.om.OverlayInfo;
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.provider.Settings;
 import android.view.View;
@@ -30,6 +33,8 @@ import android.widget.Switch;
 
 import com.android.settings.R;
 import com.android.settings.core.instrumentation.InstrumentedDialogFragment;
+
+import com.syberia.internal.util.NavBarUtils;
 
 /**
  * Dialog to set the back gesture's sensitivity in Gesture navigation mode.
@@ -64,6 +69,7 @@ public class GestureNavigationBackSensitivityDialog extends InstrumentedDialogFr
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final IOverlayManager overlayManager = IOverlayManager.Stub.asInterface(ServiceManager.getService(Context.OVERLAY_SERVICE));
         final View view = getActivity().getLayoutInflater().inflate(
                 R.layout.dialog_back_gesture_options, null);
         final SeekBar seekBarSensitivity = view.findViewById(R.id.back_sensitivity_seekbar);
@@ -80,6 +86,16 @@ public class GestureNavigationBackSensitivityDialog extends InstrumentedDialogFr
                 mArrowSwitchChecked = arrowSwitch.isChecked() ? true : false;
             }
         });
+        Switch mNavBarGesturalHideNav = view.findViewById(R.id.nav_bar_gestural_hide_nav_switch);
+        boolean mIsGesturalNavBarHidden = NavBarUtils.isGesturalNavBarHidden(getContext(), USER_CURRENT);
+        if (SystemNavigationPreferenceController.isEdgeToEdgeEnabled(getContext())) {
+            OverlayInfo ovInfo = null;
+            try {
+                ovInfo = overlayManager.getOverlayInfo(NavBarUtils.NAV_BAR_GESTURAL_HIDE_NAV_OVERLAY, USER_CURRENT);
+            } catch (RemoteException e) { }
+            mIsGesturalNavBarHidden = mIsGesturalNavBarHidden && ovInfo != null && (ovInfo.state == OverlayInfo.STATE_ENABLED);
+        }
+        mNavBarGesturalHideNav.setChecked(NavBarUtils.isGesturalNavBarHidden(getContext(), USER_CURRENT));
         return new AlertDialog.Builder(getContext())
                 .setTitle(R.string.back_options_dialog_title)
                 .setMessage(R.string.back_sensitivity_dialog_message)
@@ -91,7 +107,14 @@ public class GestureNavigationBackSensitivityDialog extends InstrumentedDialogFr
                     getArguments().putInt(KEY_BACK_HEIGHT, height);
                     SystemNavigationGestureSettings.setBackHeight(getActivity(), height);
                     SystemNavigationGestureSettings.setBackSensitivity(getActivity(),
-                            getOverlayManager(), sensitivity);
+ 				overlayManager, sensitivity);
+                    final boolean mNavBarGesturalHideNavEnabled = mNavBarGesturalHideNav.isChecked();
+                    if (SystemNavigationPreferenceController.isEdgeToEdgeEnabled(getContext())) {
+                        if (NavBarUtils.setGesturalNavBarHiddenOverlay(overlayManager, USER_CURRENT, mNavBarGesturalHideNavEnabled)) {
+                            Settings.System.putIntForUser(getContext().getContentResolver(),
+                                    Settings.System.NAV_BAR_GESTURAL_HIDE_NAV, mNavBarGesturalHideNavEnabled ? 1 : 0, USER_CURRENT);
+                        }
+                    }
                     Settings.Secure.putInt(getActivity().getContentResolver(),
                             Settings.Secure.SHOW_BACK_ARROW_GESTURE, mArrowSwitchChecked ? 1 : 0);
                 })

src/com/android/settings/gestures/SystemNavigationGestureSettings.java
@@ -55,6 +55,8 @@ import com.android.settings.widget.VideoPreference;
 import com.android.settingslib.search.SearchIndexable;
 import com.android.settingslib.widget.CandidateInfo;
 
+import com.syberia.internal.util.NavBarUtils;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -286,12 +288,15 @@ public class SystemNavigationGestureSettings extends RadioButtonPickerFragment {
             case KEY_SYSTEM_NAV_GESTURAL:
                 int sensitivity = getBackSensitivity(context, overlayManager);
                 setNavBarInteractionMode(overlayManager, BACK_GESTURE_INSET_OVERLAYS[sensitivity]);
+                NavBarUtils.setGesturalNavBarHiddenOverlay(overlayManager, USER_CURRENT, NavBarUtils.isGesturalNavBarHidden(context, USER_CURRENT));
                 break;
             case KEY_SYSTEM_NAV_2BUTTONS:
                 setNavBarInteractionMode(overlayManager, NAV_BAR_MODE_2BUTTON_OVERLAY);
+                NavBarUtils.setGesturalNavBarHiddenOverlay(overlayManager, USER_CURRENT, false);
                 break;
             case KEY_SYSTEM_NAV_3BUTTONS:
                 setNavBarInteractionMode(overlayManager, NAV_BAR_MODE_3BUTTON_OVERLAY);
+                NavBarUtils.setGesturalNavBarHiddenOverlay(overlayManager, USER_CURRENT, false);
                 break;
         }
     }